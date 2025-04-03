7cc7b9fe5e0326cb36336865e2757d98fd3f16ff
markbulavchyk <marksaitiev@gmail.com>
2025-04-02 20:14:37 -0600

    components and elements in React

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Header} from './App';
 
 const text = 'Hello world';
 
-const elem = (
-    <div>
-        <h2 className='text'>текст: {text}</h2>
-        <input type="text" />
-        <label htmlFor=""></label>
-        <button tabIndex={0}>click</button>
-    </div>
-);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    elem
+    <App/>
 );
